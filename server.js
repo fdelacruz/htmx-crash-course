a161c5a6c7540536f8256ff766b22ba2e46f8cde
Francisco De La Cruz <helloquico@gmail.com>
2025-06-21 13:15:14 +0200

    Simple request with hx-get & hx-trigger

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.use(express.urlencoded({ extended: true }));
 // parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+// HTMX-friendly proxy route
+app.get('/users', async (req, res) => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await response.json()
+
+  const html = users.map(user => `
+      <div >
+        ${user.name}
+      </div>
+    `).join('');
+
+  res.send(html);
+});
+
 // start the server
 app.listen(3000, () => {
   console.log('server listening on port 3000!');

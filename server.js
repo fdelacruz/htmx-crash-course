0833693fee4f40a323fb1a5a1dee8062ee9cba54
Francisco De La Cruz <helloquico@gmail.com>
2025-06-21 14:21:33 +0200

    hx-vals

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(express.json());
 // HTMX-friendly proxy route
 app.get('/users', async (req, res) => {
   setTimeout(async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const limit = +req.query.limit || 10
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users?_limit=${limit}`);
     const users = await response.json()
 
     const html = users.map(user => `

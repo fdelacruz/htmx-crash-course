7e4df8264e61ec4e02aa6ad603bf23ef84b5a31e
Francisco De La Cruz <helloquico@gmail.com>
2025-06-23 16:14:37 +0200

    Backend route for search

--- server.js
+++ server.js
@@ -11,14 +11,37 @@ app.use(express.urlencoded({ extended: true }));
 // parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-let currentTemperature = 35
+// handle POST request for contact search from jsonplaceholder
+app.post('/search/api', async (req, res) => {
+  const searchTerm = req.body.search.toLowerCase()
 
-// handle GET request for weather 
-app.get('/get-temperature', (req, res) => {
-  currentTemperature += Math.random() * 2 - 1 // random temp change
-  res.send(currentTemperature.toFixed(1) + '°C')
+  if (!searchTerm) {
+    return res.send('<tr></tr>')
+  }
+
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users`
+  )
+
+  const contacts = await response.json()
+
+  const searchResults = contacts.filter(contact => {
+    const name = contact.name.toLowerCase()
+    const email = contact.email.toLowerCase()
+
+    return name.includes(searchTerm) || email.includes(searchTerm)
+  })
+
+  setTimeout(() => {
+    const searchResultHTML = searchResults.map(contact => `
+    <tr>
+      <td><div class="my-4 p-2">${contact.name}</div> </td>
+      <td><div class="my-4 p-2">${contact.email}</div> </td>
+    </tr>
+    `).join('')
+    res.send(searchResultHTML)
+  }, 1000)
 })
-
 // start the server
 app.listen(3000, () => {
   console.log('server listening on port 3000!');

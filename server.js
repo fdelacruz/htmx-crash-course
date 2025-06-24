c44bff5f541a3d4ac6016d267ad0de70d8666afd
Francisco De La Cruz <helloquico@gmail.com>
2025-06-24 20:46:00 +0200

    Backend route for validation

--- server.js
+++ server.js
@@ -11,37 +11,48 @@ app.use(express.urlencoded({ extended: true }));
 // parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-// handle POST request for contact search from jsonplaceholder
-app.post('/search/api', async (req, res) => {
-  const searchTerm = req.body.search.toLowerCase()
+// handle POST request for email validation
+app.post('/contact/email', (req, res) => {
+  const submittedEmail = req.body.email
+  const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/
 
-  if (!searchTerm) {
-    return res.send('<tr></tr>')
+  const isValid = {
+    message: 'That email is valid',
+    class: 'text-green-700'
   }
 
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/users`
-  )
+  const isInValid = {
+    message: 'Please enter a valid email address',
+    class: 'text-red-700'
+  }
 
-  const contacts = await response.json()
-
-  const searchResults = contacts.filter(contact => {
-    const name = contact.name.toLowerCase()
-    const email = contact.email.toLowerCase()
-
-    return name.includes(searchTerm) || email.includes(searchTerm)
-  })
-
-  setTimeout(() => {
-    const searchResultHTML = searchResults.map(contact => `
-    <tr>
-      <td><div class="my-4 p-2">${contact.name}</div> </td>
-      <td><div class="my-4 p-2">${contact.email}</div> </td>
-    </tr>
-    `).join('')
-    res.send(searchResultHTML)
-  }, 1000)
+  if (!emailRegex.test(submittedEmail)) {
+    return res.send(
+      `
+        <div class="mb-4" hx-target="this" hx-swap="outerHTML">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email Address</label>
+          <input name="email" hx-post="/contact/email"
+            class="border rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500" type="email" value=${submittedEmail} id="email"
+            required />
+        <div class="${isInValid.class}">${isInValid.message}</div>
+        </div>
+      `
+    )
+  } else {
+    return res.send(
+      `
+        <div class="mb-4" hx-target="this" hx-swap="outerHTML">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email Address</label>
+          <input name="email" hx-post="/contact/email"
+            class="border rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500" type="email" value=${submittedEmail} id="email"
+            required />
+        <div class="${isValid.class}">${isValid.message}</div>
+        </div>
+      `
+    )
+  }
 })
+
 // start the server
 app.listen(3000, () => {
   console.log('server listening on port 3000!');
